31f8f3c168d52f6023da3e85e192b4504a917d15
5h15h1r <shishir.agrahari14@gmail.com>
2023-01-26 20:18:49 +0530

    final deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 require('express-async-errors')
 
 const express = require('express')
+const path = require('path') 
 const app = express()
 app.use(express.json())
 
@@ -21,6 +22,25 @@ const port = process.env.PORT | 3001
 app.use('/api/v1/user',userRoutes)
 app.use('/api/v1/chat', chatRoutes)
 app.use('/api/v1/message', msgRoutes)
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+// console.log(path.resolve(__dirname1, '../frontend', 'build', 'index.html'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1,'../frontend' , "/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, '../frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 
 app.use(notFound)
 app.use(errorHandler)
